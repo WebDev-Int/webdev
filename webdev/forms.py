94cbb93d2e957e1de8224e9b2c93485ec8b1178e
Craig R Powell <Powell.craig1974@gmail.com>
2022-06-13 21:57:57 -0700

    got the basics of the profile done need to work on styling with crispy

--- webdev/forms.py
+++ webdev/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from .models import Tickets
+from .models import Tickets, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 
 
 class adding_new_ticket_form(forms.ModelForm):
@@ -22,3 +25,25 @@ form = adding_new_ticket_form(forms.ModelForm)
 class login_form(forms.Form):
     username = forms.CharField(max_length=50)
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+# Create a UserUpdateForm to update username and email
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+# Create a ProfileUpdateForm to update image
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
